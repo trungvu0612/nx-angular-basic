eccf4c11b755dd2408097c69168c0011a2d3bcf2
Vu Thanh Trung <trung.vu-thanh@internal.banvien.com>
2022-08-08 15:12:36 +0700

    initial commit

---
+++ apps/nx-angular-basic/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-angular-basic-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nx-angular-basic';
+}
